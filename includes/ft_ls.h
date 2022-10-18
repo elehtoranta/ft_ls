58f5a2d57861b7b4041119e6dcad847e0bb5f42f
Erkka Lehtoranta <elehtora@student.hive.fi>
2022-10-18 06:31:26 +0300

    Norminette for header

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 14:28:36 by elehtora          #+#    #+#             */
-/*   Updated: 2022/10/18 06:13:38 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/10/18 06:31:14 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@
 # define O_ALL		0x0004
 # define O_REV		0x0008
 # define O_TIME		0x0010
-
 
 // Masking for time option selected. The time displayed in
 // long format can represent time modified, created or last
